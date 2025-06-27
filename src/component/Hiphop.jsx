e6414ec3f48c98a03cd1d430fe041124b96080f7
707g <daandaan707@gmail.com>
2025-06-28 02:43:24 +0900

    hiphop list

--- src/component/Hiphop.jsx
+++ src/component/Hiphop.jsx
@@ -4,44 +4,93 @@ import { useProState } from '../stateContext';
 import { Link } from 'react-router-dom';
 
 const ItemsWrapper = styled.div`
+  width: 1350px;
+  margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  box-sizing: border-box;
-  margin-bottom: 100px;
-  max-width: 1350px;
-  margin-left: auto;
-  margin-right: auto;
+`;
+
+const HiphopHeader = styled.div`
+  width: 100%;
+  height: 350px;
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+  position: relative;
+
+  img {
+    width: 1350px;
+    height: 350px;
+    border-radius: 5px;
+  }
+`;
+
+const OverText = styled.div`
+  position: absolute;
+  top: 55%; 
+  left: 80%;
+  transform: translate(-50%, -50%);
+  color: #ffffff;
+  text-align: center;
+
+  h1 {
+    font-size: 40px;
+    margin: 0;
+    font-weight: bold;
+    text-shadow: 2px 2px 5px rgba(0,0,0,0.7);
+  }
+`;
+
+const ItemWrap = styled.div`
+  width: 1350px;
+  margin: 15px auto 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 const ItemBlock = styled.div`
-  width: calc(25% - 20px);
-  margin: 30px 10px 0 10px;
-  box-sizing: border-box;
+  width: 310px;
+  margin-top: 30px;
   display: flex;
   flex-direction: column;
-  justify-content: start;
   align-items: center;
-  height: 320px;
+  text-align: center;
 
   img {
-    width: 70%;
-    margin: 15px 0 auto;
+    width: 310px;
+    height: 270px;
+    object-fit: contain;
+    margin: 20px 0 10px 0;
     cursor: pointer;
+    border-radius: 5px;
+    transition: transform 0.3s ease;
+    &:hover {
+      transform: scale(1.02);
+      cursor: pointer;
+      box-shadow: 0 8px 15px rgba(0,0,0,0.2);
+    }
   }
+
   h4 {
+    width: 100%;
+    font-size: 15px;
+    color: #6B6B6B;
+    margin: 0 0 8px;
+    padding: 0 5px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  p {
+    width: 100%;
     font-size: 14px;
     color: #6B6B6B;
-    text-align: center;
-    padding: 0 5px;
-    flex-grow: 1;
-  }
-  p {
-    font-size: 13px;
-    color: #6B6B6B;
+    margin: 0;
   }
 `;
-
 
 function Hiphop() {
   const state = useProState();
@@ -49,16 +98,23 @@ function Hiphop() {
 
   return (
     <ItemsWrapper>
-      {hiphopList.map((item) => (
-        <ItemBlock key={item.id}>
-          <Link to={`/component/SubHiphop/${item.id}`} className='no-underline'>
-            <img src={process.env.PUBLIC_URL + item.src} alt={item.ttl} />
-          </Link>
-          <h4>{item.ttl}</h4>
-          <p>{item.price}</p>
-          
-        </ItemBlock>
-      ))}
+      <HiphopHeader>
+        <img src={process.env.PUBLIC_URL + "/images/hiphop/mainImg.png"} alt="mainImg" />
+        <OverText>
+          <h1>HIPHOP/SOUL/R&B</h1>
+        </OverText>
+      </HiphopHeader>
+      <ItemWrap>
+        {hiphopList.map((item) => (
+          <ItemBlock key={item.id}>
+            <Link to={`/component/SubHiphop/${item.id}`} className='no-underline'>
+              <img src={process.env.PUBLIC_URL + item.src} alt={item.ttl} />
+            </Link>
+            <h4>{item.ttl}</h4>
+            <p>{item.price}</p>      
+          </ItemBlock>
+        ))}
+      </ItemWrap>
     </ItemsWrapper>
   );
 }
